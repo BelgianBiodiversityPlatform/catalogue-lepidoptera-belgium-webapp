66713fd752fca65b55112f1cc7ef4c8e45ec35a3
Nicolas Noé <nicolas@niconoe.eu>
2018-01-17 11:00:02 +0100

    Some basic models + Admin.

--- website/lepidoptera/admin.py
+++ website/lepidoptera/admin.py
@@ -1,8 +1,27 @@
 from django.contrib import admin
-from .models import Family
+from .models import Family, Species, Province, TimePeriod, SpeciesPresence
 
 
+@admin.register(Family)
 class FamilyAdmin(admin.ModelAdmin):
     pass
 
-admin.site.register(Family, FamilyAdmin)
+
+@admin.register(Species)
+class SpeciesAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Province)
+class ProvinceAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(TimePeriod)
+class TimePeriodAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(SpeciesPresence)
+class SpeciesPresencePeriodAdmin(admin.ModelAdmin):
+    pass
--- website/lepidoptera/models.py
+++ website/lepidoptera/models.py
@@ -8,3 +8,23 @@ class Family(models.Model):
 
     class Meta:
         verbose_name_plural = "families"
+
+
+class Species(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Province(models.Model):
+    name = models.CharField(max_length=255)
+    code = models.CharField(max_length=3)
+
+
+class TimePeriod(models.Model):
+    name = models.CharField(max_length=255)
+    icon = models.ImageField()
+
+
+class SpeciesPresence(models.Model):
+    species = models.ForeignKey(Species, on_delete=models.CASCADE)
+    province = models.ForeignKey(Province, on_delete=models.CASCADE)
+    period = models.ForeignKey(TimePeriod, on_delete=models.CASCADE)
