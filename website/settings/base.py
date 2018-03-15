e1902d01dd9c68ef063d44e1b4808ba374f68b12
Nicolas Noé <nicolas@niconoe.eu>
2018-03-15 12:58:10 +0100

    Moved Djangpo-debug-toolbar settings to development settings

--- website/settings/base.py
+++ website/settings/base.py
@@ -34,7 +34,6 @@ INSTALLED_APPS = [
 
     'markdownx',
     'imagekit',
-#    'debug_toolbar',
     'denorm',
 
     'lepidoptera'
@@ -50,8 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.locale.LocaleMiddleware',
 
-    'denorm.middleware.DenormMiddleware',
-#    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    'denorm.middleware.DenormMiddleware'
 ]
 
 ROOT_URLCONF = 'website.urls'
@@ -135,19 +133,3 @@ SETTINGS_EXPORT = [
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
-
-DEBUG_TOOLBAR_PANELS = [
-    'debug_toolbar.panels.versions.VersionsPanel',
-    'debug_toolbar.panels.timer.TimerPanel',
-    'debug_toolbar.panels.settings.SettingsPanel',
-    'debug_toolbar.panels.headers.HeadersPanel',
-    'debug_toolbar.panels.request.RequestPanel',
-    'debug_toolbar.panels.sql.SQLPanel',
-    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
-    'debug_toolbar.panels.templates.TemplatesPanel',
-    'debug_toolbar.panels.cache.CachePanel',
-    'debug_toolbar.panels.signals.SignalsPanel',
-    'debug_toolbar.panels.logging.LoggingPanel',
-    'debug_toolbar.panels.redirects.RedirectsPanel',
-    'debug_toolbar.panels.profiling.ProfilingPanel',
-]
