bfb2c1172a8145654cd8ff45df3f7db96d340216
Nicolas Noé <nicolas@niconoe.eu>
2019-08-27 09:56:04 +0200

    Skip Bandit check after careful review.

--- lepidoptera/utils.py
+++ lepidoptera/utils.py
@@ -2,4 +2,5 @@ import subprocess
 
 
 def get_source_version_info():
-    return subprocess.run(['git', 'log', "--pretty=format:%h (%ai)", '-n 1'], stdout=subprocess.PIPE).stdout.decode('utf-8')
+    return subprocess.run(['git', 'log', "--pretty=format:%h (%ai)", '-n 1'],  # nosec
+                          stdout=subprocess.PIPE).stdout.decode('utf-8')  # nosec
